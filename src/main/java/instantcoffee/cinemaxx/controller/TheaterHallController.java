3b076cd24829f7a7cb8e6bbeb7181b8c48781528
Veselin Ivanov <v3s1ez@gmail.com>
2021-10-17 15:23:50 +0200

    test endpoint

--- src/main/java/instantcoffee/cinemaxx/controller/TheaterHallController.java
+++ src/main/java/instantcoffee/cinemaxx/controller/TheaterHallController.java
@@ -1,9 +1,22 @@
 package instantcoffee.cinemaxx.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import instantcoffee.cinemaxx.authentication.User;
 
 @RestController
 @RequestMapping("/theater")
 public class TheaterHallController {
+  // NOTE(v3s1e): This is how you get the user inside your request :):w
+  @GetMapping
+  public String hello(@AuthenticationPrincipal User user) {
+    System.out.println("HELLO");
+    System.out.println(user);
+    return "hello";
+  }
 }
