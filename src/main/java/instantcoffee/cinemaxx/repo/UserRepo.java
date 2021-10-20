cdc051c534e9a2dc905c94240e5bf800547afab3
andr01a0 <andr01a0@stud.kea.dk>
2021-10-20 21:26:44 +0200

    delete userRepo

--- src/main/java/instantcoffee/cinemaxx/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package instantcoffee.cinemaxx.repo;
-
-import instantcoffee.cinemaxx.authentication.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
