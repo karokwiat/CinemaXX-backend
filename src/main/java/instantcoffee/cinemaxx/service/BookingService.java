d6b93abb9572075b53736c6621f1ddc662c2a5c6
andr01a0 <andr01a0@stud.kea.dk>
2021-10-20 21:24:04 +0200

    booking endpoint

---
+++ src/main/java/instantcoffee/cinemaxx/service/BookingService.java
@@ -0,0 +1,16 @@
+package instantcoffee.cinemaxx.service;
+
+import instantcoffee.cinemaxx.dto.MovieDTO;
+import instantcoffee.cinemaxx.dto.MovieDTOCustomer;
+import instantcoffee.cinemaxx.dto.MovieDTODate;
+import instantcoffee.cinemaxx.entities.Movie;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface BookingService {
+
+    public void cancelBooking(int id);
+
+}
---
+++ src/main/java/instantcoffee/cinemaxx/service/BookingServiceImp.java
@@ -0,0 +1,25 @@
+package instantcoffee.cinemaxx.service;
+
+import instantcoffee.cinemaxx.dto.MovieDTO;
+import instantcoffee.cinemaxx.dto.MovieDTOCustomer;
+import instantcoffee.cinemaxx.dto.MovieDTODate;
+import instantcoffee.cinemaxx.entities.Movie;
+import instantcoffee.cinemaxx.error.ResourceNotFoundException;
+import instantcoffee.cinemaxx.repo.MovieRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class BookingServiceImp implements BookingService{
+
+    BookingService bookingService;
+
+    @Override
+    public void cancelBooking(int id) {
+        System.out.println("cancel booking.");
+    }
+
+}
