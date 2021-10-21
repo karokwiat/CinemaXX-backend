6ef20332b769e499ce81d818ca01246f37ddee50
Karolina Kwiatkowska <48014678+karokwiat@users.noreply.github.com>
2021-10-21 12:41:32 +0200

    Update Booking.java

--- src/main/java/instantcoffee/cinemaxx/entities/Booking.java
+++ src/main/java/instantcoffee/cinemaxx/entities/Booking.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @RequiredArgsConstructor
-@Table(name = "booking")
+@Table(name = "bookings")
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
